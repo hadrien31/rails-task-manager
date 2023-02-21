246f0c19613c9b287deabfd0cfbfac348a852178
hadrien mari <hadrienmari@yahoo.fr>
2023-02-21 17:02:20 +0100

    exo2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
 
+  get 'tasks', to:"tasks#index"
+  get "tasks/:id", to: "tasks#show", as: :task
   # Defines the root path route ("/")
   # root "articles#index"
+  get "tasks/:id/edit", to: "tasks#edit", as: :tasks_edit
+  patch "tasks/:id", to: "tasks#update"
+
+  delete "tasks/:id", to: "tasks#destroy"
 end
